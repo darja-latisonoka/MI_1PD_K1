3d30d41db30d6b4daa0f76e225ec3aeb43fa6589
Areulius <arelisvaldis@gmail.com>
2026-03-03 12:22:04 +0200

    improved formatting, removed unnecessary self.

--- pages/GamePage.py
+++ pages/GamePage.py
@@ -5,12 +5,8 @@ class GamePage(tk.Frame):
 	def __init__(self, parent, app):
 		super().__init__(parent)
 
-		self.textLabel = ttk.Label(self, text="THIS IS WHERE GAME HAPPENS")
-		self.textLabel.place(relx=0.5, rely=0.5, anchor="center")
+		textLabel = ttk.Label(self, text="THIS IS WHERE GAME HAPPENS")
+		textLabel.place(relx=0.5, rely=0.5, anchor="center")
 
-		self.backBtn = ttk.Button(
-			self,
-			text="BACK (to main menu)",
-			command=lambda: app.show_page("MainMenuPage")
-		)
-		self.backBtn.place(relx=0.5, rely=0.9, anchor="center")
+		backBtn = ttk.Button(self, text="BACK (to main menu)", command=lambda: app.show_page("MainMenuPage"))
+		backBtn.place(relx=0.5, rely=0.9, anchor="center")
--- pages/MainMenuPage.py
+++ pages/MainMenuPage.py
@@ -5,11 +5,11 @@ class MainMenuPage(tk.Frame):
 	def __init__(self, parent, app):
 		super().__init__(parent)
 		
-		self.startBtn = ttk.Button(self, text='Play', command=lambda: app.new_round())
-		self.startBtn.place(relx=0.5, rely=0.5, anchor="center")
+		startBtn = ttk.Button(self, text='PLAY', command=lambda: app.new_round())
+		startBtn.place(relx=0.5, rely=0.5, anchor="center")
 
-		self.infoBtn = ttk.Button(self, text='Info', command=lambda: app.show_page("InfoPage"))
-		self.infoBtn.place(relx=0.5, rely=0.6, anchor="center")
+		infoBtn = ttk.Button(self, text='INFO', command=lambda: app.show_page("InfoPage"))
+		infoBtn.place(relx=0.5, rely=0.6, anchor="center")
 
-		self.close_button = ttk.Button(self, text='Exit', command=app.destroy)
-		self.close_button.place(relx=0.5, rely=0.9, anchor="center")
+		close_button = ttk.Button(self, text='EXIT', command=app.destroy)
+		close_button.place(relx=0.5, rely=0.9, anchor="center")
